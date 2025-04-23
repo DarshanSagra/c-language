60531319a42a98d0421f47c36b71aa0dbf4081d5
VatsalPrajapati2005 <kamleshprajapti101947>
2025-04-23 10:39:49 +0530

    project1c

---
+++ task1/p1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+    // float PI;
+    // int r;
+    // PI=3.14;
+    // printf("enter value R => ");
+    // scanf("%d",&r);
+
+    // printf("%2f",PI*r*r);
+
+
+    // int l;
+    // printf("enter value l=> ");
+    // scanf("%d",&l);
+    // printf("%d",l*l);
+
+
+    // float half=0.5;
+    // int b,h;
+    // printf("enter value b=> ");
+    // scanf("%d",&b);
+    // printf("enter value h=> ");
+    // scanf("%d",&h);
+
+    // printf("%2f",half*(b*h));
+
+
+
+    // int p,r,n;
+    // printf("enter value P=> ");
+    //  scanf("%d",&p);
+    // printf("enter value R=> ");
+    //  scanf("%d",&r);
+    //  printf("enter value N=> ");
+    //  scanf("%d",&n);
+
+    //  printf("%d",(p*r*n)/100);
+
+
+    // float PI =3.14;
+    // int r ;
+    // printf("enter value R=> ");
+    // scanf("%d",&r);
+
+    //  printf("%2f",2*PI*r);
+    
+}
---
+++ task1/p2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    
+
+    //Task 1
+
+    int a,b;
+    printf("enter value a => ");
+    scanf("%d",&a);
+    printf("enter value b =>");
+    scanf("%d",&b);
+
+    printf("\nAddition =>%d",a+b);
+    printf("\nSubtraction =>%d",a-b);
+    printf(" \nMultiplication =>%d",a*b);
+    printf("\nDivison =>%d",a/b);
+    printf("\nModule =>%d",a%b);
+
+
+    //Task 2
+
+//     int A=5;
+//     int B=5;
+//     printf("First patten\n");
+//     printf("- - - - - - - -\n");
+//    printf("|             |\n");
+//    printf("|             |\n");
+//    printf("| 5 x 5 = %d  |\n",A*B);
+//    printf("|             |\n");
+//    printf("|             |\n");
+//     printf("- - - - - - - -\n");
+
+
+}
